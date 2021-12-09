2d7e8c3c7b78f4882692053f506468f58dfa69eb
hafsaxfarid <59932046+hafsaxfarid@users.noreply.github.com>
2021-12-09 17:59:21 -0500

    Activation Particles
    Added activation particles to checkpoints

--- HF_GAME2014_Lab11/Assets/Scripts/CheckpointController.cs
+++ HF_GAME2014_Lab11/Assets/Scripts/CheckpointController.cs
@@ -5,19 +5,30 @@ using UnityEngine;
 public class CheckpointController : MonoBehaviour
 {
     public Transform spawnPoint;
-
     private GameController gameController;
+
+    [Header("Enemy Explode")]
+    public ParticleSystem checkpointActivated;
+    public Color activatedColor;
 
     void Start()
     {
         gameController = FindObjectOfType<GameController>();
+        checkpointActivated = GetComponentInChildren<ParticleSystem>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
             gameController.setCurrentSpawnPoint(spawnPoint);
+            CheckPointActive();
         }
+    }
+
+    private void CheckPointActive()
+    {
+        checkpointActivated.GetComponent<Renderer>().material.SetColor("_Color", activatedColor);
+        checkpointActivated.Play();
     }
 }
